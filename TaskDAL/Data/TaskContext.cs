8b151672cd434953c53076774b1350c8e95188b6
Sahak Sargsyan <sargsyan_sahak@yahoo.com>
2024-05-09 05:04:35 +0400

    TaskContext(DbContext) file

---
+++ TaskDAL/Data/TaskContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskDAL.Entities;
+
+namespace TaskDAL
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Entities.Task>? Tasks { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // the actual connection string should be hidden, this is just a fake connection string for now
+            optionsBuilder.UseSqlServer(
+                @"Server=dev;Database=dev;User Id=dev;Password=dev;");
+        }
+
+        // Configuring the tables using Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // one-to-many relationship between User and Task
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Tasks)
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.UserId);
+
+            // one-to-many relationship between Category and Task
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Tasks)
+                .WithOne(t => t.Category)
+                .HasForeignKey(t => t.CategoryId);
+        }
+    }
+}
