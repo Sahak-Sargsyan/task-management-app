492eff7d588bac840220d2ad940486d7bdb20687
Sahak Sargsyan <sargsyan_sahak@yahoo.com>
2024-05-09 16:28:28 +0400

    The main buisness logic added, under development

---
+++ TaskBLL/Services/TaskService.cs
@@ -0,0 +1,88 @@
+﻿using TaskBLL.Interfaces;
+using TaskBLL.Models;
+using TaskDAL.Repositories;
+using Task = System.Threading.Tasks.Task;
+using TaskEntity = TaskDAL.Entities.Task;
+
+namespace TaskBLL.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly TaskRepository _taskRepository;
+        public TaskService(TaskRepository taskRepository)
+        {
+            this._taskRepository = taskRepository;
+        }
+
+        public async Task AddAsync(TaskModel model)
+        {
+            var taskList = await _taskRepository.GetAllAsync();
+            var last = taskList.LastOrDefault();
+            var newIndex = last == null ? 1 : last.Id + 1;
+
+            var taskEntity = new TaskEntity(model.Id, model.Title, model.Description, model.DueDate, model.Priority, model.CategoryId, model.UserId);
+
+            await _taskRepository.AddAsync(taskEntity);
+        }
+
+        public async Task DeleteAsync(int modelId)
+        {
+            await _taskRepository.DeleteByIdAsync(modelId);
+        }
+
+        public async Task<IEnumerable<TaskModel>> GetAllAsync()
+        {
+            var TaskEntityList = await _taskRepository.GetAllWithDetails();
+            var taskModelList = new List<TaskModel>();
+
+            foreach (var taskEntity in TaskEntityList)
+            {
+                var taskModel = new TaskModel(
+                    taskEntity.Id, 
+                    taskEntity.Title, 
+                    taskEntity.Description, 
+                    taskEntity.DueDate, 
+                    taskEntity.Priority, 
+                    taskEntity.CategoryId, 
+                    taskEntity.UserId);
+            }
+
+            return taskModelList;
+        }
+
+        public async Task<TaskModel> GetByIdAsync(int id)
+        {
+            var taskEntity = await _taskRepository.GetByIdWithDetails(id);
+            var taskModel = new TaskModel(taskEntity.Id, taskEntity.Title, taskEntity.Description, taskEntity.DueDate, taskEntity.Priority, taskEntity.CategoryId, taskEntity.UserId);
+
+            return taskModel;
+        }
+
+        public async Task<IEnumerable<TaskModel>> GetTasksByCategory(int categoryId)
+        {
+            var taskEnitityList = await _taskRepository.GetAllByCategoryId(categoryId);
+            var taskModelList = new List<TaskModel>();
+
+            if (taskEnitityList != null)
+            {
+                foreach(var taskEntity in taskEnitityList)
+                {
+                    taskModelList.Add(new TaskModel(taskEntity.Id,taskEntity.Title,taskEntity.Description, taskEntity.DueDate, taskEntity.Priority, taskEntity.Category.Id, taskEntity.UserId));
+                }
+            }
+
+            return taskModelList;
+        }
+
+        public async Task UpdateAsync(TaskModel model)
+        {
+            var taskToUpdate = await _taskRepository.GetByIdAsync(model.Id);
+            if(taskToUpdate == null)
+            {
+                throw new ArgumentNullException(nameof(model), "No such task to update");
+            }
+
+            await _taskRepository.UpdateAsync(taskToUpdate);
+        }
+    }
+}
---
+++ TaskBLL/Services/UserService.cs
@@ -0,0 +1,91 @@
+﻿using TaskBLL.Helpers;
+using TaskBLL.Interfaces;
+using TaskBLL.Models;
+using TaskDAL.Entities;
+using TaskDAL.Repositories;
+using Task = System.Threading.Tasks.Task;
+
+namespace TaskBLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserRepository _userRepository;
+        public UserService(UserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+        public async Task AddAsync(UserModel model)
+        {
+            if (_userRepository.GetUserByUserName(model.UserName) == null)
+            {
+                var userList = await _userRepository.GetAllAsync();
+                var last = userList.LastOrDefault();
+                var newIndex = last == null ? 1 : last.Id + 1;
+                // The key should not be here
+                string password = EncryptionHelper.Encrypt(model.Password, "bcndhkth78qiiopp");
+
+                var user = new User(newIndex, model.Name, model.UserName, model.Email, password);
+
+                await _userRepository.AddAsync(user);
+            }
+        }
+
+        public async Task DeleteAsync(int modelId)
+        {
+            var user = await _userRepository.GetByIdAsync(modelId);
+            await _userRepository.DeleteAsync(user);
+        }
+
+        public async Task<IEnumerable<UserModel>> GetAllAsync()
+        {
+            var userList = await _userRepository.GetAllWithDetails();
+            var userModelList = new List<UserModel>();
+
+            foreach (var user in userList)
+            {
+                var userModel = new UserModel(user.Id, user.Name, user.UserName, user.Email, user.Password);
+                if (user.Tasks != null)
+                {
+                    foreach(var task in user.Tasks)
+                    {
+                        userModel.TaskIds.Add(task.Id);
+                    }
+                }
+                userModelList.Add(userModel);
+            }
+
+            return userModelList;
+        }
+
+        public async Task<UserModel> GetByIdAsync(int id)
+        {
+            var user = await _userRepository.GetByIdWithDetails(id);
+            var userModel = new UserModel(user.Id, user.Name, user.UserName, user.Email, user.Password);
+
+            if (user.Tasks != null)
+            {
+                foreach (var task in user.Tasks)
+                {
+                    userModel.TaskIds.Add(task.Id);
+                }
+            }
+
+            return userModel;
+        }
+
+        public async Task UpdateAsync(UserModel model)
+        {
+            var user = await _userRepository.GetByIdWithDetails(model.Id);
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(model), "No such user to update");
+            }
+
+            user.UserName = model.UserName;
+            user.Email = model.Email;
+            user.Name = model.Name;
+
+            await _userRepository.UpdateAsync(user);
+        }
+    }
+}
