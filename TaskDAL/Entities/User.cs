d6f7a20ac6f21fd286f629fdd1f94f5e548bb8e0
Sahak Sargsyan <sargsyan_sahak@yahoo.com>
2024-05-18 00:08:41 +0400

    removing Categories table and some changes in all entities

--- TaskDAL/Entities/User.cs
+++ TaskDAL/Entities/User.cs
@@ -1,20 +1,18 @@
 ﻿namespace TaskDAL.Entities
 {
+
     public class User
     {
-        public User(int id, string name, string userName, string email, string password)
+        public User(string name, string userName, string password)
         {
-            Id = id;
             Name = name;
             UserName = userName;
-            Email = email;
             Password = password;
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
         public string UserName { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
 
         public virtual ICollection<Task> Tasks { get; set; }
