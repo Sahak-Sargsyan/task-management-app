e22732d0cd77812cf39fb1711af1b0185af2c0e6
Sahak Sargsyan <sargsyan_sahak@yahoo.com>
2024-05-18 00:09:53 +0400

    Adding XML comments to all stuff

--- TaskDAL/Repositories/TaskRepository.cs
+++ TaskDAL/Repositories/TaskRepository.cs
@@ -3,30 +3,56 @@ using TaskEntity = TaskDAL.Entities.Task;
 using Task = System.Threading.Tasks.Task;
 using TaskDAL.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using TaskDAL.Entities;
 
 namespace TaskDAL.Repositories
 {
+    /// <summary>
+    /// Contains operations working with Tasks
+    /// </summary>
     public class TaskRepository : AbstractRepository, ITaskRepository
     {
+        /// <summary>
+        /// DbSet type of <see cref="TaskEntity"/> to work with Tasks table
+        /// </summary>
         private readonly DbSet<TaskEntity> _tasks;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="TaskRepository"/> with <see cref="TaskContext"/> dbContext
+        /// </summary>
+        /// <param name="context">TaskContext dbContext</param>
         public TaskRepository(TaskContext context) : base(context)
         {
             _tasks = _context.Set<TaskEntity>();
         }
 
+        /// <summary>
+        /// Adds given <see cref="TaskEntity"/> entity into Tasks table
+        /// </summary>
+        /// <param name="entity">Task entity</param>
+        /// <returns></returns>
         public async Task AddAsync(TaskEntity entity)
         {
             await _tasks.AddAsync(entity);
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Removes specified <see cref="TaskEntity"/> entity from table
+        /// </summary>
+        /// <param name="entity">Task entity</param>
+        /// <returns></returns>
         public async Task DeleteAsync(TaskEntity entity)
         {
             _tasks.Remove(entity);
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Removes Task with specified id
+        /// </summary>
+        /// <param name="id">Id of specified Task</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task DeleteByIdAsync(int id)
         {
             var task = await _tasks.FindAsync(id);
@@ -39,11 +65,21 @@ namespace TaskDAL.Repositories
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Returns all rows from Tasks table
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<TaskEntity>> GetAllAsync()
         {
             return await _tasks.ToListAsync();
         }
 
+        /// <summary>
+        /// Returns the specified number of rows
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="rowCount"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<TaskEntity>> GetAllAsync(int pageNumber, int rowCount)
         {
             return await _tasks.Skip((pageNumber - 1) * rowCount)
@@ -51,21 +87,24 @@ namespace TaskDAL.Repositories
                         .ToListAsync();
         }
 
-        public async Task<ICollection<TaskEntity>> GetAllByCategoryId(int categoryId)
+        /// <summary>
+        /// Returns all Tasks of specified user
+        /// </summary>
+        /// <param name="userId">Id for user</param>
+        /// <returns></returns>
+        public async Task<ICollection<TaskEntity>> GetAllByUserId(int userId)
         {
-            var list = await _tasks.Where(t => t.CategoryId == categoryId).ToListAsync();
-            if (list == null)
-            {
-                list = new List<TaskEntity>();
-            }
-
-            return list;
+            return await _tasks.Where(t => t.UserId == userId).ToListAsync();
         }
 
+        /// <summary>
+        /// Returns all rows with details
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">throws the exception when there is nothing to return</exception>
         public async Task<ICollection<TaskEntity>> GetAllWithDetails()
         {
-            var tasks = await _tasks.Include(t => t.Category)
-                                    .Include(t => t.User)
+            var tasks = await _tasks.Include(t => t.User)
                                     .ToListAsync();
             if(tasks == null)
             {
@@ -75,6 +114,12 @@ namespace TaskDAL.Repositories
             return tasks;
         }
 
+        /// <summary>
+        /// Returns Task with specified id
+        /// </summary>
+        /// <param name="id">Id for task</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">throws the exception when task doesn't exist</exception>
         public async Task<TaskEntity> GetByIdAsync(int id)
         {
             var task = await _tasks.FindAsync(id);
@@ -86,10 +131,15 @@ namespace TaskDAL.Repositories
             return task;
         }
 
+        /// <summary>
+        /// Returns Task with details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Throws the exception when task doesn't exist</exception>
         public async Task<TaskEntity> GetByIdWithDetails(int id)
         {
-            var entityWithDetails = await _tasks.Include(t => t.Category)
-                                                .Include(t => t.User).FirstOrDefaultAsync(task => task.Id == id);
+            var entityWithDetails = await _tasks.Include(t => t.User).FirstOrDefaultAsync(task => task.Id == id);
             if(entityWithDetails == null)
             {
                 throw new ArgumentNullException(nameof(entityWithDetails), "The entity trying to get with details is null");
@@ -98,10 +148,18 @@ namespace TaskDAL.Repositories
             return entityWithDetails;
         }
 
+        /// <summary>
+        /// Updates the specified <see cref="TaskEntity"/> entity in the table
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task UpdateAsync(TaskEntity entity)
         {
-            _tasks.Attach(entity);
-            _context.Entry(entity).State = EntityState.Modified;
+            var existingEntity = _tasks.Find(entity.Id);
+            if (existingEntity != null)
+            {
+                _context.Entry(existingEntity).CurrentValues.SetValues(entity);
+            }
             await _context.SaveChangesAsync();
         }
     }
--- TaskDAL/Repositories/UserRepository.cs
+++ TaskDAL/Repositories/UserRepository.cs
@@ -5,26 +5,53 @@ using Task = System.Threading.Tasks.Task;
 
 namespace TaskDAL.Repositories
 {
+    /// <summary>
+    /// Contains operations working with Users
+    /// </summary>
     public class UserRepository : AbstractRepository, IUserRepository
     {
+        /// <summary>
+        /// DbSet field type of <see cref="User"/> to work with Users table
+        /// </summary>
         private readonly DbSet<User> _users;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="UserRepository"/> with <see cref="TaskContext"/> dbContext
+        /// </summary>
+        /// <param name="context">dbContext parameter</param>
         public UserRepository(TaskContext context) : base(context)
         {
             _users = _context.Set<User>();
         }
 
+        /// <summary>
+        /// Adds the specified <see cref="User"/> entity to Users table
+        /// </summary>
+        /// <param name="entity">User entity</param>
+        /// <returns></returns>
         public async Task AddAsync(User entity)
         {
             await _users.AddAsync(entity);
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Removes the specified <see cref="User"/> entity from Users table
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task DeleteAsync(User entity)
         {
             _users.Remove(entity);
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Removes the user given by id
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task DeleteByIdAsync(int id)
         {
             var entity = await _users.FindAsync(id);
@@ -37,11 +64,21 @@ namespace TaskDAL.Repositories
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Returns all rows of Users table
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<User>> GetAllAsync()
         {
             return await _users.ToListAsync();
         }
 
+        /// <summary>
+        /// Returns specified count rows of Users table
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="rowCount"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<User>> GetAllAsync(int pageNumber, int rowCount)
         {
             return await _users.Skip((pageNumber - 1) * rowCount)
@@ -49,6 +86,11 @@ namespace TaskDAL.Repositories
                         .ToListAsync();
         }
 
+        /// <summary>
+        /// Returns specified count rows from Users table
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task<ICollection<User>> GetAllWithDetails()
         {
             var users = await _users.Include(u => u.Tasks).ToListAsync();
@@ -60,6 +102,12 @@ namespace TaskDAL.Repositories
             return users;
         }
 
+        /// <summary>
+        /// Returns user with specified id
+        /// </summary>
+        /// <param name="id">User Id</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">throws the exception when there is nothing to return</exception>
         public async Task<User> GetByIdAsync(int id)
         {
             var user = await _users.FindAsync(id);
@@ -71,6 +119,12 @@ namespace TaskDAL.Repositories
             return user;
         }
 
+        /// <summary>
+        /// Returns the user with details
+        /// </summary>
+        /// <param name="id">user id</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task<User> GetByIdWithDetails(int id)
         {
             var user = await _users.Include(u => u.Tasks).FirstOrDefaultAsync();
@@ -82,11 +136,21 @@ namespace TaskDAL.Repositories
             return user;
         }
 
+        /// <summary>
+        /// Returns user by specified user name
+        /// </summary>
+        /// <param name="userName">Users's name</param>
+        /// <returns></returns>
         public async Task<User> GetUserByUserName(string userName)
         {
             return await _users.FirstOrDefaultAsync(u => u.UserName == userName);
         }
 
+        /// <summary>
+        /// Updates the specified <see cref="User"/> entity in the table
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task UpdateAsync(User entity)
         {
             _users.Attach(entity);
